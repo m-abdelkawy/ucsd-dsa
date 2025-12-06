12a0bb37963bf969859ecb4c74f7f8274eacc4b0
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2025-12-06 10:25:07 -0500

    commit first task to try the grading tool, a simple method that adds two digits

--- course1_toolbox/src/main/java/edu/ucsd/dsa/toolbox/Main.java
+++ course1_toolbox/src/main/java/edu/ucsd/dsa/toolbox/Main.java
@@ -1,4 +1,4 @@
-package edu.ucsd.dsa;
+package edu.ucsd.dsa.toolbox;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
